6629a2455cbc61ab0952c1059c0ce58c905103fb
shortcuts <vannicattec@gmail.com>
2025-04-02 11:58:24 +0200

    chore: create file

--- pkg/cmd/transformations/setup/setup.go
+++ pkg/cmd/transformations/setup/setup.go
@@ -2,6 +2,7 @@ package setup
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/MakeNowJust/heredoc"
 	"github.com/algolia/algoliasearch-client-go/v4/algolia/ingestion"
@@ -12,7 +13,6 @@ import (
 	"github.com/algolia/cli/pkg/cmdutil"
 	"github.com/algolia/cli/pkg/config"
 	"github.com/algolia/cli/pkg/iostreams"
-	"github.com/algolia/cli/pkg/printers"
 	"github.com/algolia/cli/pkg/validators"
 )
 
@@ -102,54 +102,9 @@ func runNewCmd(opts *NewOptions) error {
 
 	opts.Sample = resp.GetData()[0]
 
-	if err := opts.IO.StartPager(); err != nil {
-		fmt.Fprintf(opts.IO.ErrOut, "error starting pager: %v\n", err)
-	}
-	defer opts.IO.StopPager()
-
-	table := printers.NewTablePrinter(opts.IO)
-	if table.IsTTY() {
-		table.AddField("NAME", nil, nil)
-		table.AddField("ENTRIES", nil, nil)
-		table.AddField("SIZE", nil, nil)
-		table.AddField("UPDATED AT", nil, nil)
-		table.AddField("CREATED AT", nil, nil)
-		table.AddField("LAST BUILD DURATION", nil, nil)
-		table.AddField("PRIMARY", nil, nil)
-		table.AddField("REPLICAS", nil, nil)
-		table.EndRow()
+	if err := os.Mkdir(opts.TransformationName, 0o750); err != nil {
+		return err
 	}
 
-	// for _, index := range res.Items {
-	// 	var primary string
-	// 	if index.Primary == nil {
-	// 		primary = ""
-	// 	} else {
-	// 		primary = *index.Primary
-	// 	}
-	// 	updatedAt, err := parseTime(index.UpdatedAt)
-	// 	if err != nil {
-	// 		return fmt.Errorf("can't parse %s into a time struct", index.UpdatedAt)
-	// 	}
-	// 	createdAt, err := parseTime(index.CreatedAt)
-	// 	if err != nil {
-	// 		return fmt.Errorf("can't parse %s into a time struct", index.CreatedAt)
-	// 	}
-	// 	// Prevent integer overflow
-	// 	if index.DataSize < 0 {
-	// 		index.DataSize = 0
-	// 	}
-	// 	table.AddField(index.Name, nil, nil)
-	// 	table.AddField(humanize.Comma(int64(index.Entries)), nil, nil)
-	// 	table.AddField(humanize.Bytes(uint64(index.DataSize)), nil, nil)
-	// 	table.AddField(updatedAt, nil, nil)
-	// 	table.AddField(createdAt, nil, nil)
-	// 	table.AddField(strconv.Itoa(int(index.LastBuildTimeS))+"s", nil, nil)
-	// 	table.AddField(primary, nil, nil)
-	// 	table.AddField(fmt.Sprintf("%v", index.Replicas), nil, nil)
-	// 	table.EndRow()
-	// }
-	return table.Render()
+	return nil
 }
-
-/////////////////// bubbles stuff for list selection
